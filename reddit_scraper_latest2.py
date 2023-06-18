caec1dd2ebd202331aa1ceedb597226da8491bae
rkedia1 <97908255+rkedia1@users.noreply.github.com>
2023-06-18 15:00:09 -0400

    Add files via upload
    uploading latest reddit scraper that outputs dictionary of date and sentiment score for each stock ticker.

--- reddit_scraper_latest2.py
+++ reddit_scraper_latest2.py
@@ -2,7 +2,7 @@
 """
 Created on Wed Jun  7 12:17:44 2023
 
-@author: kedia
+@author: rkedia
 """
 
 import praw
@@ -11,9 +11,35 @@ import os
 import re
 from collections import defaultdict
 from data import EquityData
+from datetime import datetime
+
+import pandas as pd
+
+
+#nltk.download('punkt')
+#nltk.download('stopwords')
+#nltk.download('wordnet')
+#nltk.download('vader_lexicon')
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.sentiment import SentimentIntensityAnalyzer
+from nltk.corpus import stopwords
+
 
 # Load environment variables from .env file
 load_dotenv()
+
+#import pdb; pdb.set_trace()
+
+#all_stocks = pd.read_csv("equities/all_stocks.csv")
+disc = pd.read_csv("equities/discretionary.csv")
+staples = pd.read_csv("equities/staples.csv")
+stocks = pd.concat([disc, staples])[["Symbol", "Market Cap"]]
+stocks = stocks[stocks['Symbol'].str.len() < 5]
+stocks['Market Cap'] = stocks['Market Cap'].apply(lambda x: float(x.replace(' M', '000000').replace(",", "")))
+stocks = stocks.sort_values('Market Cap', ascending = False)[:100]
+stocks = stocks['Symbol'].to_numpy()
+print(stocks)
 
 
 class RedditAnalyzer:
@@ -25,9 +51,41 @@ class RedditAnalyzer:
         self.username = os.getenv("REDDIT_USERNAME")
         self.password = os.getenv("REDDIT_PASSWORD")
         
+        #self.word_embeddings_model = gensim.models.Word2Vec.load('path_to_word_embeddings_model')
+        self.vader_analyzer = SentimentIntensityAnalyzer()
         self.equity_data = EquityData()
-        self.stock_tickers = self.equity_data.consumer_stocks
+        self.stock_tickers = stocks
         self.stop_words = ['I', 'AND', 'THE']
+
+
+    def tokenize(self, sentence):
+        tokens = word_tokenize(sentence)
+        return tokens
+    
+    def lemmatize(self, tokens):
+        lemmatizer = WordNetLemmatizer()
+        lemmas = [lemmatizer.lemmatize(token) for token in tokens]
+        return lemmas
+    
+    def remove_stopwords(self, tokens):
+        stopwords_list = set(stopwords.words('english'))
+        filtered_tokens = [token for token in tokens if token.lower() not in stopwords_list]
+        return filtered_tokens
+    
+    def process_sentence(self, sentence):
+        tokens = self.tokenize(sentence)
+        lemmas = self.lemmatize(tokens)
+        filtered_tokens = self.remove_stopwords(lemmas)
+        sentiment_scores = self.vader_analyzer.polarity_scores(' '.join(filtered_tokens))
+        sentiment_score = sentiment_scores['compound']
+        filtered_tokens.append(sentiment_score)
+        return filtered_tokens
+    
+    def generate_word_embeddings(self, sentence):
+        # Generate word embeddings for a given sentence using the word embeddings model
+        # You'll need to modify this based on the specific word embeddings library you're using
+        word_embeddings = self.word_embeddings_model[sentence]
+        return word_embeddings
 
     def retrieve_submission_text(self, subreddit_topic, count=500):
         reddit = praw.Reddit(
@@ -39,27 +97,59 @@ class RedditAnalyzer:
         )
         subreddit = reddit.subreddit(subreddit_topic)
         submissions = subreddit.hot(limit=count)
-        return [submission.selftext for submission in submissions if "CLICK HERE FOR" not in submission.selftext]
+        submission_data = []
+        
+        for submission in submissions:
+            if "CLICK HERE FOR" not in submission.selftext:
+                submission_data.append({'date': datetime.fromtimestamp(submission.created_utc).strftime('%Y-%m-%d'),
+                                        'text': submission.selftext})
+        
+        return submission_data
+        #return [submission.selftext for submission in submissions if "CLICK HERE FOR" not in submission.selftext]
 
     def retrieve_tickers_sentences(self, stock_posts):
-        mydict = defaultdict(list)
+        #import pdb; pdb.set_trace()
         pattern = r"\b[A-Z]{1,5}\b"
-        stock_posts = ' '.join(stock_posts)
-        sentences = re.split(r'(?<=[.?!])\s+', stock_posts)
-        for sentence in sentences:
-            tickers = re.findall(pattern, sentence)
-            for ticker in tickers:
-                if ticker in self.stock_tickers:
-                    mydict[ticker].append(sentence)
+        mydict = defaultdict(list)
+        
+        for post in stock_posts:
+            sentences = re.split(r'(?<=[.?!])\s+', post['text'])
+        #agg_posts = ' '.join([list(stock_post.values())[-1] for stock_post in stock_posts])
+        #sentences = re.split(r'(?<=[.?!])\s+', agg_posts)
+            for sentence in sentences:
+                tickers = re.findall(pattern, sentence)
+                
+                #tokens = word_tokenize(sentence)
+                #stopwords_list = set(stopwords.words('english'))
+    
+                #lower_non_stopwords = [token.lower() if not token.isupper() else token for token in tokens if token.lower() not in stopwords_list]
+                
+                #lemmatizer = WordNetLemmatizer()
+                #lemmas = [lemmatizer.lemmatize(token) for token in lower_non_stopwords]
+                sentence_tokens = self.process_sentence(sentence)
+                sentiment_scores = sentence_tokens.pop()
+                
+                
+                for ticker in tickers:
+                    if ticker in self.stock_tickers:
+                        mydict[ticker].append(
+                            {'date': post['date'],
+                            'sentiment_score': sentiment_scores})
         return mydict
 
 
 reddit_analyzer = RedditAnalyzer()
-stock_posts = reddit_analyzer.retrieve_submission_text("stocks")
-investing_posts = reddit_analyzer.retrieve_submission_text("investing")
-wsb_posts = reddit_analyzer.retrieve_submission_text("wallstreetbets")
+stock_posts = reddit_analyzer.retrieve_submission_text("stocks", count = 500)
+investing_posts = reddit_analyzer.retrieve_submission_text("investing", count = 500)
+wsb_posts = reddit_analyzer.retrieve_submission_text("wallstreetbets", count = 500)
 
 posts = stock_posts + investing_posts + wsb_posts
 
 ticker_sentence_dict = reddit_analyzer.retrieve_tickers_sentences(posts)
 print(ticker_sentence_dict)
+
+
+
+
+
+
