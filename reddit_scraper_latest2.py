d2a15baeb2eb014d75c778d67c4562fa3a99e320
rkedia1 <97908255+rkedia1@users.noreply.github.com>
2023-06-19 17:40:43 -0400

    Updating reddit scraper file to include sentiment score

--- reddit_scraper_latest2.py
+++ reddit_scraper_latest2.py
@@ -11,8 +11,8 @@ import os
 import re
 from collections import defaultdict
 from data import EquityData
-from datetime import datetime
-
+from datetime import datetime, timedelta
+import yaml
 import pandas as pd
 
 
@@ -31,15 +31,8 @@ load_dotenv()
 
 #import pdb; pdb.set_trace()
 
-#all_stocks = pd.read_csv("equities/all_stocks.csv")
-disc = pd.read_csv("equities/discretionary.csv")
-staples = pd.read_csv("equities/staples.csv")
-stocks = pd.concat([disc, staples])[["Symbol", "Market Cap"]]
-stocks = stocks[stocks['Symbol'].str.len() < 5]
-stocks['Market Cap'] = stocks['Market Cap'].apply(lambda x: float(x.replace(' M', '000000').replace(",", "")))
-stocks = stocks.sort_values('Market Cap', ascending = False)[:100]
-stocks = stocks['Symbol'].to_numpy()
-print(stocks)
+
+ 
 
 
 class RedditAnalyzer:
@@ -54,8 +47,30 @@ class RedditAnalyzer:
         #self.word_embeddings_model = gensim.models.Word2Vec.load('path_to_word_embeddings_model')
         self.vader_analyzer = SentimentIntensityAnalyzer()
         self.equity_data = EquityData()
-        self.stock_tickers = stocks
+        #self.stock_tickers = stocks
         self.stop_words = ['I', 'AND', 'THE']
+        
+    
+    @staticmethod
+    def get_top_stock_symbols(stock_count = 100):
+        
+        #universe of consumer discretionary and consumer staples stocks
+        disc = pd.read_csv("equities/discretionary.csv")
+        staples = pd.read_csv("equities/staples.csv")
+        
+        #concatenate into one dataframe
+        stocks = pd.concat([disc, staples])[["Symbol", "Market Cap"]]
+        
+        #stocks only with 4 letters or less
+        stocks = stocks[stocks['Symbol'].str.len() < 5]
+        
+        #replace 'M' with 6 zeros and cast string as float
+        stocks['Market Cap'] = stocks['Market Cap'].apply(lambda x: float(x.replace(' M', '000000').replace(",", "")))
+        
+        
+        stocks = stocks.sort_values('Market Cap', ascending = False)[:stock_count]
+        stocks = stocks['Symbol'].to_numpy()
+        return stocks
 
 
     def tokenize(self, sentence):
@@ -73,11 +88,23 @@ class RedditAnalyzer:
         return filtered_tokens
     
     def process_sentence(self, sentence):
+        
+        #tokenize
         tokens = self.tokenize(sentence)
+        
+        #lemmatize
         lemmas = self.lemmatize(tokens)
+        
+        #remove stopwords
         filtered_tokens = self.remove_stopwords(lemmas)
+        
+        #dictionary of sentiment score attributes for string of text 
         sentiment_scores = self.vader_analyzer.polarity_scores(' '.join(filtered_tokens))
+        
+        #retunrns just a single, compound sentiment score
         sentiment_score = sentiment_scores['compound']
+        
+        
         filtered_tokens.append(sentiment_score)
         return filtered_tokens
     
@@ -87,7 +114,9 @@ class RedditAnalyzer:
         word_embeddings = self.word_embeddings_model[sentence]
         return word_embeddings
 
-    def retrieve_submission_text(self, subreddit_topic, count=500):
+    def retrieve_submission_text(self, subreddit_topic, timefilter = 'year'):
+        
+        #reddit instance
         reddit = praw.Reddit(
             client_id=self.client_id,
             client_secret=self.client_secret,
@@ -95,8 +124,10 @@ class RedditAnalyzer:
             username=self.username,
             password=self.password
         )
+        
+        #start_time = datetime.utcnow() - timedelta(days=365 * 2)
         subreddit = reddit.subreddit(subreddit_topic)
-        submissions = subreddit.hot(limit=count)
+        submissions = subreddit.hot(limit = 10000)
         submission_data = []
         
         for submission in submissions:
@@ -119,19 +150,12 @@ class RedditAnalyzer:
             for sentence in sentences:
                 tickers = re.findall(pattern, sentence)
                 
-                #tokens = word_tokenize(sentence)
-                #stopwords_list = set(stopwords.words('english'))
-    
-                #lower_non_stopwords = [token.lower() if not token.isupper() else token for token in tokens if token.lower() not in stopwords_list]
-                
-                #lemmatizer = WordNetLemmatizer()
-                #lemmas = [lemmatizer.lemmatize(token) for token in lower_non_stopwords]
                 sentence_tokens = self.process_sentence(sentence)
                 sentiment_scores = sentence_tokens.pop()
                 
                 
                 for ticker in tickers:
-                    if ticker in self.stock_tickers:
+                    if ticker in RedditAnalyzer.get_top_stock_symbols():
                         mydict[ticker].append(
                             {'date': post['date'],
                             'sentiment_score': sentiment_scores})
@@ -139,17 +163,18 @@ class RedditAnalyzer:
 
 
 reddit_analyzer = RedditAnalyzer()
-stock_posts = reddit_analyzer.retrieve_submission_text("stocks", count = 500)
-investing_posts = reddit_analyzer.retrieve_submission_text("investing", count = 500)
-wsb_posts = reddit_analyzer.retrieve_submission_text("wallstreetbets", count = 500)
+stock_posts = reddit_analyzer.retrieve_submission_text("stocks")
+investing_posts = reddit_analyzer.retrieve_submission_text("investing")
+wsb_posts = reddit_analyzer.retrieve_submission_text("wallstreetbets")
 
 posts = stock_posts + investing_posts + wsb_posts
 
 ticker_sentence_dict = reddit_analyzer.retrieve_tickers_sentences(posts)
-print(ticker_sentence_dict)
+#print(ticker_sentence_dict)
 
 
-
+with open('reddit_scores.yaml', 'w') as file:
+    yaml.dump(ticker_sentence_dict, file)
 
 
 
