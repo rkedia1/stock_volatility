97b0c18c94770adaf17d9c5304432d2b6eefdb73
Nicholas Keller <nkeller97@gmail.com>
2023-06-22 19:37:50 -0400

    Added rolling timeseries train test split

--- helpers.py
+++ helpers.py
@@ -2,7 +2,7 @@
 
 
 import pandas as pd
-
+from sklearn.model_selection import TimeSeriesSplit
 
 def err_handle(e: Exception,
                file: str):
@@ -63,5 +63,29 @@ def remove_duplicate_index(df: pd.DataFrame,
     if priority not in ['first', 'last']:
         raise AttributeError(priority, 'Not in first or last')
     return df.loc[~df.index.duplicated(keep=priority)]
+
+def time_series_split(X: pd.DataFrame, Y: pd.DataFrame, n_splits: int = 5):
+    tscv = TimeSeriesSplit(n_splits)
+    X = X.reset_index()
+    Y = Y.reset_index()
+    X[["year", "month", "day", "ticker"]] = X['index'].apply(lambda x: pd.Series(str(x).split("_")))
+    Y[["year", "month", "day", "ticker"]] = Y['index'].apply(lambda x: pd.Series(str(x).split("_")))
+    X['date'] = pd.to_datetime(X[['year', 'month', 'day']])
+    Y['date'] = pd.to_datetime(Y[['year', 'month', 'day']])
+    X = X.set_index("index")
+    Y = Y.set_index("index")
+    X = X.sort_values(by=["date", "ticker"])
+    Y = Y.sort_values(by=["date", "ticker"])
+    dates = X['date'].unique()
+    for train_index, test_index in tscv.split(dates):
+        train_date_start = dates[train_index[0]].date()
+        train_date_end = dates[train_index[-1]].date()
+        test_date_start = dates[test_index[0]].date()
+        test_date_end = dates[test_index[-1]].date()
+        X_train = X[(X['date'].dt.date >= train_date_start) & (X['date'].dt.date <= train_date_end)][X.columns[:2]]
+        Y_train = Y[(Y['date'].dt.date >= train_date_start) & (Y['date'].dt.date <= train_date_end)][Y.columns[:2]]
+        X_test = X[(X['date'].dt.date >= test_date_start) & (X['date'].dt.date <= test_date_end)][X.columns[:2]]
+        Y_test = Y[(Y['date'].dt.date >= test_date_start) & (Y['date'].dt.date <= test_date_end)][Y.columns[:2]]
+        yield X_train, Y_train, X_test, Y_test
 
 
