570fe0e0ed0356d9e6ec2fa329d35b5cd2683f1f
rkedia1 <97908255+rkedia1@users.noreply.github.com>
2023-06-25 15:41:44 -0400

    shapely - might work

---
+++ shapely_values_.py
@@ -0,0 +1,69 @@
+
+import pandas as pd
+
+from supervised_ensemble import Model
+
+from helpers import merge_dataframes
+from sklearn.ensemble import RandomForestRegressor
+
+import shap
+import numpy as np
+
+
+def create_datasets(m: Model):
+    datasets = m.create_technical_datasets()
+    X, Y = m.create_X_Y(datasets)
+
+    bollinger_X = m.create_objective(X.copy(), 'bollinger')
+    sentiment_dataset = m.apply_sentiment_data(X.copy(), m.sentiment_dataset)
+    sentiment_dataset.dropna(inplace=True)
+
+    applied_dataset = m.apply_sentiment_data(bollinger_X.copy(), m.sentiment_dataset)
+    applied_dataset.dropna(inplace=True)
+
+    return {'basic_sentiment': sentiment_dataset,
+            'combined_tehcnical': applied_dataset,
+            'Y': Y}
+
+
+def create_train_test(m: Model,
+                      X: pd.DataFrame,
+                      Y: pd.DataFrame):
+    X = pd.DataFrame(X)
+    Y = pd.DataFrame(Y)
+    x_cols = X.columns
+    y_cols = Y.columns
+
+    dataset = merge_dataframes([X, Y]).dropna()
+    X = pd.DataFrame(dataset[x_cols])
+    Y = pd.DataFrame(dataset[y_cols])
+
+    xtrain, xtest, ytrain, ytest = m.temporal_train_test_split(X, Y)
+    xtrain = xtrain.loc[xtrain.index.isin(ytrain.index)]
+    ytrain = ytrain.loc[ytrain.index.isin(xtrain.index)]
+    xtest = xtest.loc[xtest.index.isin(ytest.index)]
+    ytest = ytest.loc[ytest.index.isin(xtest.index)]
+
+    return xtrain, xtest, ytrain, ytest
+
+
+
+if __name__ == '__main__':
+    m = Model()
+    datasets = create_datasets(m)
+    print(datasets)
+
+    # CHOOSE whatever model; setting RF
+    ensemble = RandomForestRegressor()
+
+    xtrain, xtest, ytrain, ytest = create_train_test(m, datasets['basic_sentiment'], datasets['Y'])
+
+    ensemble.fit(xtrain, ytrain)
+    pred = pd.DataFrame(ensemble.predict(xtest), index=xtest.index)
+
+
+    explainer = shap.Explainer(ensemble)
+    shap_values = explainer.shap_values(xtest)
+
+    shap.summary_plot(shap_values, xtest, feature_names = xtest.columns)
+
